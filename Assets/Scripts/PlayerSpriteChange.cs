e8fb5e913133b0cc85513889ff3b5dfe0794d329
9rotama <aoim99.sc@gmail.com>
2022-02-18 02:32:05 +0900

    AI作成とプレイヤー画像設定

---
+++ Assets/Scripts/PlayerSpriteChange.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpriteChange : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite spriteSpeed0;
+    public Sprite spriteSpeed1;
+    public Sprite spriteSpeed2;
+
+    private Vector3 prevPosition;
+    
+    void Start()
+    {
+        prevPosition = transform.position;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        var velocityVec2 = (transform.position - prevPosition) / Time.deltaTime;
+        var velocity = Math.Sqrt(Math.Pow(velocityVec2.x, 2) + Math.Pow(velocityVec2.y, 2));
+        prevPosition = transform.position;
+        // 移動速度計算
+
+        Debug.Log(velocity);
+    }
+}
