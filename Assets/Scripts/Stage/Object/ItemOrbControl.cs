a81ba8fab2e5e15548ff86c25bf59958303d8f5e
meg <65887771+9rotama@users.noreply.github.com>
2023-06-12 19:49:09 +0900

    [fix] itemOrbが回収後再生成されるように修正

--- Assets/Scripts/Stage/Object/ItemOrbControl.cs
+++ Assets/Scripts/Stage/Object/ItemOrbControl.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class ItemOrbControl : MonoBehaviour, IRacerCollisionEnterer
 {
     [SerializeField] private ItemDecider itemDecider;
+    private OrbSpawner _parentOrbSpawner;
+
 
     public void OnTriggerEnterRacer(Racer racer)
     {
         itemDecider.DecideItem(racer);
+        _parentOrbSpawner.OrbDestroyed();
         Destroy(this.gameObject);
+    }
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _parentOrbSpawner = transform.parent.GetComponent<OrbSpawner>();
     }
 
 }
